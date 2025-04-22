83215b9d3b9e4f5b88d3f944fb204a444cec29b5
Joirid Juárez <a00835769@tec.mx>
2025-04-22 16:12:29 -0600

    Sorted List

--- cypress/e2e/FormFill.cy.js
+++ cypress/e2e/FormFill.cy.js
@@ -1,5 +1,8 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    // Entrar en la pantalla de Yellow or Blue desde la pantalla de inicio
+    cy.visit('https://thelab.boozang.com/')
+    cy.get('.veggie_burger').click()
+    cy.get(':nth-child(4) > .sub_list > :nth-child(2) > span > .link').click()
   })
 })
--- cypress/e2e/SortedList.cy.js
+++ cypress/e2e/SortedList.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    // Entrar en la pantalla de Yellow or Blue desde la pantalla de inicio
+    // Entrar en la pantalla de Sorted list desde la pantalla de inicio
     cy.visit('https://thelab.boozang.com/')
     cy.get('.veggie_burger').click()
     cy.get(':nth-child(4) > .sub_list > :nth-child(1) > span > .link').click()
