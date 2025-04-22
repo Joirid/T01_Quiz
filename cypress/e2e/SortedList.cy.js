e9b59c0a9502593c78ffb5e87c10e8634035835f
Joirid Juárez <a00835769@tec.mx>
2025-04-22 14:54:30 -0600

    Sorted list

--- cypress/e2e/SortedList.cy.js
+++ cypress/e2e/SortedList.cy.js
@@ -1,5 +1,16 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    // Entrar en la pantalla de Yellow or Blue desde la pantalla de inicio
+    cy.visit('https://thelab.boozang.com/')
+    cy.get('.veggie_burger').click()
+    cy.get(':nth-child(4) > .sub_list > :nth-child(1) > span > .link').click()
+
+    // Agregar dos elementos a la lista
+    cy.get('input').type('Elemento 1', { delay: 100 })
+    cy.get('.add').click()
+    cy.wait(1000) // Esperar un segundo para que el elemento se agregue a la lista  
+
+    cy.get('input').type('Elemento 2', { delay: 100 })
+    cy.get('.add').click()
   })
 })
